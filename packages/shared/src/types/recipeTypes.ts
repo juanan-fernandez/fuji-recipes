b9199a1bb9932f916572e7866f6c75a6ab15c77d
juanan fernandez <juanan.fernandez@gmail.com>
2025-12-26 14:26:26 +0100

    chore: update shared package, recipes router and draft controller

---
+++ packages/shared/src/types/recipeTypes.ts
@@ -0,0 +1,22 @@
+export interface Recipe {
+   id: number;
+   recipeName: string;
+   author: string;
+   url: string;
+   cameraSensor: string;
+   film: string;
+   grain: string;
+   colorChromeEffect: string;
+   colorChromeFxBlue: string;
+   whiteBalance: string;
+   dynamicRange: number;
+   highlight: number;
+   shadow: number;
+   color: number;
+   sharpness: number;
+   nr: number;
+   clarity: number;
+   expCompensation: string;
+   iso: string;
+   notes: string;
+}
